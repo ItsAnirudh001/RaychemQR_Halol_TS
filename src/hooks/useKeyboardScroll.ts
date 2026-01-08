3d14a062e9c03553d7e4d3e854eb7597c1502200
anirudh-ahana <macairbookm1@gmail.com>
2026-01-08 06:18:43 +0530

    Mobile Keyboard scroll update

--- src/hooks/useKeyboardScroll.ts
+++ src/hooks/useKeyboardScroll.ts
@@ -1,12 +1,14 @@
 import { handleInputScroll } from "@/utils/helpers";
 import { useEffect } from "react";
+import { isMobile } from "react-device-detect";
 
 export function useKeyboardScroll() {
   useEffect(() => {
     const { visualViewport } = window;
-    if (!visualViewport) return;
+    if (!visualViewport || !isMobile) return;
 
     visualViewport.addEventListener("resize", handleInputScroll);
-    return () => visualViewport.removeEventListener("resize", handleInputScroll);
+    return () =>
+      visualViewport.removeEventListener("resize", handleInputScroll);
   }, []);
 }
