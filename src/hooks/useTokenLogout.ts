1579ac898cd378f7cc3b5a58de0e5f1429e462ba
anirudh-ahana <macairbookm1@gmail.com>
2026-04-21 09:07:13 +0530

    Reverted token logout

--- src/hooks/useTokenLogout.ts
+++ src/hooks/useTokenLogout.ts
@@ -1,60 +1,101 @@
 "use client";
 
 import { checkToken } from "@/utils/helpers";
-import { updateLogoutMode } from "@/utils/session-utils";
+import { getStoredUser, updateLogoutMode } from "@/utils/session-utils";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
+// import dayjs from "dayjs";
+// import utc from "dayjs/plugin/utc";
+
+// dayjs.extend(utc);
+
+// export default function useTokenLogout() {
+//   const { push } = useRouter();
+//   const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+//   function handleBeforeUnload(e: BeforeUnloadEvent) {
+//     e.preventDefault();
+//   }
+
+//   function logout() {
+//     updateLogoutMode("token");
+//     push("/");
+//   }
+
+//   async function tokenLogout() {
+//     const token = getStoredUser()?.access_token;
+//     if (intervalRef.current || !token)
+//       return clearInterval(intervalRef.current!);
+
+//     const interval = 1000;
+//     const tokenExpiry = checkToken();
+//     const expirytime = dayjs.utc(tokenExpiry).format("HH:mm:ss A");
+
+//     intervalRef.current = setInterval(() => {
+//       const currentTime = dayjs().format("HH:mm:ss A");
+
+//       console.log("expiryTime X", expirytime);
+//       console.log("currentTime X", currentTime);
+//       // console.log("timePassed", timePassed);
+
+//       // console.log(
+//       //   "currenttime",
+//       //   dayjs(currentTime).format("DD-MM-YY hh:mm:ss"),
+//       // );
+//       // console.log(
+//       //   "expirytime",
+//       //   dayjs(expirytime).format("DD-MM-YY hh:mm:ss"),
+//       //   checkToken(),
+//       // );
+
+//       if (currentTime >= expirytime) {
+//         clearInterval(intervalRef.current!);
+//         logout();
+//       }
+//     }, interval);
+//   }
+
+//   useEffect(() => {
+//     window.addEventListener("beforeunload", handleBeforeUnload);
+//     window.addEventListener("login", tokenLogout);
+
+//     return () => {
+//       window.removeEventListener("login", tokenLogout);
+//       window.removeEventListener("beforeunload", handleBeforeUnload);
+
+//       if (intervalRef.current) clearInterval(intervalRef.current);
+//     };
+//   }, []);
+// }
 
 export default function useTokenLogout() {
   const { push } = useRouter();
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-  const logoutTimeRef = useRef<number>(0);
 
   function handleBeforeUnload(e: BeforeUnloadEvent) {
+    const user = getStoredUser();
+    if (!user.user_id) return;
+
     e.preventDefault();
   }
 
-  function logout() {
-    updateLogoutMode("token");
-    push("/");
-  }
+  async function tokenLogout() {
+    const remainingTime = checkToken();
 
-  function tokenLogout() {
-    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    console.log("token expiry time", remainingTime);
 
-    const logoutTime = checkToken();
-    logoutTimeRef.current = logoutTime;
-
-    // console.log("logoutTime", logoutTime);
-
-    const loginTime = Date.now();
-    localStorage.setItem("login_time", loginTime.toString());
-
-    timeoutRef.current = setTimeout(logout, logoutTime);
-  }
-
-  function handleVisibilityChange() {
-    if (document.hidden) return;
-
-    const loginTime = localStorage.getItem("login_time");
-
-    if (!loginTime) return;
-
-    const elapsedTime = Date.now() - parseInt(loginTime);
-    if (elapsedTime >= logoutTimeRef.current) logout();
+    setTimeout(() => {
+      updateLogoutMode("token");
+      push("/");
+    }, remainingTime);
   }
 
   useEffect(() => {
     window.addEventListener("beforeunload", handleBeforeUnload);
     window.addEventListener("login", tokenLogout);
-    document.addEventListener("visibilitychange", handleVisibilityChange);
 
     return () => {
       window.removeEventListener("login", tokenLogout);
       window.removeEventListener("beforeunload", handleBeforeUnload);
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-
-      if (timeoutRef.current) clearTimeout(timeoutRef.current);
     };
   }, []);
 }
